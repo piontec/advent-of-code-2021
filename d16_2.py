e876ee3e681aedf001bec242f676437460028d0c
Łukasz Piątkowski <piontec@gmail.com>
2021-12-17 13:57:06 +0100

    d16

--- d16_2.py
+++ d16_2.py
@@ -1,3 +1,4 @@
+import math
 from typing import Tuple
 
 from bitarray import bitarray
@@ -57,54 +58,73 @@ def decode_literal_value(val: bitarray) -> (int, int):
     return res, processed_bits
 
 
-def parse_packets(bits: bitarray, start_index: int, packets_count: int = 0, packets_length: int = 0) -> Tuple[int, int]:
+def parse_packets(bits: bitarray, start_index: int, packets_count: int = 0, packets_length: int = 0) -> Tuple[
+                  int, list[int, int]]:
     processed_packets = 0
     index = start_index
-    ver_sum = 0
+    values: list[int] = []
     while not (
             (processed_packets > 0 and processed_packets == packets_count) or (index > 0 and index == packets_length)):
-        if len(bits[index:]) <= 7 and not(bits[index:].any()):
+        if len(bits[index:]) <= 7 and not (bits[index:].any()):
             # i hope it's padding
             break
         version = bitarray_to_int(bits[index:index + 3])
-        ver_sum += version
         type_id = bitarray_to_int(bits[index + 3:index + 6])
         index += 6
-        packets_count += 1
+        processed_packets += 1
         if type_id == 4:
             val, fwd = decode_literal_value(bits[index:])
             index += fwd
+            values.append(val)
         else:
             mode = bits[index]
             index += 1
             if mode == 0:
                 total_len = bitarray_to_int(bits[index:index + 15])
                 index += 15
-                ind_add, sum_add = parse_packets(bits[index:], 0, packets_length=total_len)
+                ind_add, vals = parse_packets(bits[index:], 0, packets_length=total_len)
             else:
                 packet_count = bitarray_to_int(bits[index:index + 11])
                 index += 11
-                ind_add, sum_add = parse_packets(bits[index:], 0, packets_count=packet_count)
+                ind_add, vals = parse_packets(bits[index:], 0, packets_count=packet_count)
             index += ind_add
-            ver_sum += sum_add
-    return index, ver_sum
+            match type_id:
+                case 0:
+                    values.append(sum(vals))
+                case 1:
+                    values.append(math.prod(vals))
+                case 2:
+                    values.append(min(vals))
+                case 3:
+                    values.append(max(vals))
+                case 5:
+                    values.append(1 if vals[0] > vals[1] else 0)
+                case 6:
+                    values.append(1 if vals[0] < vals[1] else 0)
+                case 7:
+                    values.append(1 if vals[0] == vals[1] else 0)
+                case _:
+                    raise Exception("WTF?")
+
+    return index, values
 
 
 def run(line: str) -> int:
     bits = hex_str_to_bitarray(line)
-    ind, ver_sum = parse_packets(bits, 0, packets_count=1)
-    return ver_sum
+    ind, values = parse_packets(bits, 0, packets_count=1)
+    assert len(values) == 1
+    return values[0]
 
 
 def test() -> None:
-    # test_val = hex_str_to_bitarray("D2FE28")[6:]
-    # assert decode_literal_value(test_val) == (2021, 18)
-    test_val = hex_str_to_bitarray("38006F45291200")
-    parse_packets(test_val, 0, packets_count=1)
-    assert run("8A004A801A8002F478") == 16
-    assert run("620080001611562C8802118E34") == 12
-    assert run("C0015000016115A2E0802F182340") == 23
-    assert run("A0016C880162017C3686B18A3D4780") == 31
+    # assert run("C200B40A82") == 3
+    # assert run("04005AC33890") == 54
+    # assert run("880086C3E88112") == 7
+    # assert run("CE00C43D881120") == 9
+    # assert run("D8005AC2A8F0") == 1
+    # assert run("F600BC2D8F") == 0
+    # assert run("9C005AC2F8F0") == 0
+    assert run("9C0141080250320F1802104A08") == 1
 
 
 if __name__ == "__main__":
