750aa50bf34dc476066daf59f8b2f39ae848f14b
Łukasz Piątkowski <piontec@gmail.com>
2021-12-27 14:54:41 +0100

    d24 progress

--- d24_1.py
+++ d24_1.py
@@ -1,87 +1,86 @@
-from dataclasses import dataclass
-from typing import Tuple
-
-import numpy as np
-
-Point = Tuple[int, int]
-Fold = Tuple[str, int]
-
-Range = Tuple[int, int]
+from typing import TypedDict
 
 
-@dataclass
-class Cuboid:
-    on: bool
-    x: Range
-    y: Range
-    z: Range
+class Regs(TypedDict):
+    w: int
+    x: int
+    y: int
+    z: int
+
+
+class Alu:
+    def __init__(self, inp: list[int]):
+        self.inp = inp
+        self.regs: Regs = {"w": 0, "x": 0, "y": 0, "z": 0}
+        self._inp_pointer = 0
+
+    def _arg_val(self, arg: str) -> int:
+        if arg in ["w", "x", "y", "z"]:
+            return self.regs[arg]
+        return int(arg)
+
+    def run_instr(self, instr: str) -> bool:
+        op, args = instr.split(" ", maxsplit=1)
+        r1 = args[0]
+        if r1 not in ["w", "x", "y", "z"]:
+            raise Exception("bad arg[0]")
+        match op:
+            case "inp":
+                self.regs[r1] = self.inp[self._inp_pointer]
+                self._inp_pointer += 1
+            case "add":
+                r2 = args.split(" ")[1]
+                self.regs[r1] = self.regs[r1] + self._arg_val(r2)
+            case "mul":
+                r2 = args.split(" ")[1]
+                self.regs[r1] = self.regs[r1] * self._arg_val(r2)
+            case "div":
+                r2 = args.split(" ")[1]
+                av = self._arg_val(r2)
+                if av == 0:
+                    self.regs["z"] = 1
+                    return False
+                self.regs[r1] = self.regs[r1] // av
+            case "mod":
+                r2 = args.split(" ")[1]
+                if self.regs[r1] < 0:
+                    self.regs["z"] = 2
+                    return False
+                av = self._arg_val(r2)
+                if av <= 0:
+                    self.regs["z"] = 3
+                    return False
+                self.regs[r1] = self.regs[r1] % av
+            case "eql":
+                r2 = args.split(" ")[1]
+                self.regs[r1] = 1 if self.regs[r1] == self._arg_val(r2) else 0
+            case _:
+                raise Exception("unknown op")
+        return True
+
+
+def run(lines: list[str], inp: list[int]) -> int:
+    alu = Alu(inp)
+    for line in lines:
+        if not alu.run_instr(line.strip()):
+            break
+    return alu.regs["z"]
 
 
 def main() -> None:
-    with open("i22.txt", "r") as i:
+    with open("i24.txt", "r") as i:
         lines = i.readlines()
-    res = run(lines)
+    # inp = [1, 3, 5, 7, 9, 2, 4, 6, 8, 9, 9, 9, 9, 9]
+    inp = [1, 9, 1, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
+    res = run(lines, inp)
     print(res)
 
 
-def run(lines: list[str], size: int = 101) -> int:
-    cuboids: list[Cuboid] = []
-    for line in lines:
-        on_txt, rest = line.strip().split(" ")
-        on = on_txt == "on"
-        ranges = rest.split(",")
-        x_nums = ranges[0][2:].split(".")
-        x_min, x_max = int(x_nums[0]), int(x_nums[2])
-        y_nums = ranges[1][2:].split(".")
-        y_min, y_max = int(y_nums[0]), int(y_nums[2])
-        z_nums = ranges[2][2:].split(".")
-        z_min, z_max = int(z_nums[0]), int(z_nums[2])
-        c = Cuboid(on, (x_min, x_max), (y_min, y_max), (z_min, z_max))
-        cuboids.append(c)
-    cube = np.zeros((size, size, size), dtype=np.int8)
-    offset = 50
-    range_min = - size // 2
-    range_max = size // 2
-    for c in cuboids:
-        if (c.x[0] < range_min and c.x[1] < range_min) \
-                or (c.x[0] > range_max and c.x[1] > range_max) \
-                or (c.y[0] < range_min and c.y[1] < range_min) \
-                or (c.y[0] > range_max and c.y[1] > range_max) \
-                or (c.z[0] < range_min and c.z[1] < range_min) \
-                or (c.z[0] > range_max and c.z[1] > range_max):
-            continue
-        cube[c.z[0] + offset:c.z[1] + offset + 1, c.y[0] + offset:c.y[1] + offset + 1,
-            c.x[0] + offset:c.x[1] + offset + 1] = 1 if c.on else 0
-    res = sum(cube[cube == 1])
-    return res
-
-
 def test() -> None:
-    txt = """on x=-20..26,y=-36..17,z=-47..7
-on x=-20..33,y=-21..23,z=-26..28
-on x=-22..28,y=-29..23,z=-38..16
-on x=-46..7,y=-6..46,z=-50..-1
-on x=-49..1,y=-3..46,z=-24..28
-on x=2..47,y=-22..22,z=-23..27
-on x=-27..23,y=-28..26,z=-21..29
-on x=-39..5,y=-6..47,z=-3..44
-on x=-30..21,y=-8..43,z=-13..34
-on x=-22..26,y=-27..20,z=-29..19
-off x=-48..-32,y=26..41,z=-47..-37
-on x=-12..35,y=6..50,z=-50..-2
-off x=-48..-32,y=-32..-16,z=-15..-5
-on x=-18..26,y=-33..15,z=-7..46
-off x=-40..-22,y=-38..-28,z=23..41
-on x=-16..35,y=-41..10,z=-47..6
-off x=-32..-23,y=11..30,z=-14..3
-on x=-49..-5,y=-3..45,z=-29..18
-off x=18..30,y=-20..-8,z=-3..13
-on x=-41..9,y=-7..43,z=-33..15
-on x=-54112..-39298,y=-85059..-49293,z=-27449..7877
-on x=967..23432,y=45373..81175,z=27513..53682"""
+    txt = ""
     assert run(txt.splitlines()) == 590784
 
 
 if __name__ == "__main__":
-    test()
+    # test()
     main()
