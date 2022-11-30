68a1c210353aa2c12348e8c71c3e9c4a9f46bc8a
Łukasz Piątkowski <piontec@gmail.com>
2022-11-30 09:07:14 +0100

    WIP: d21

--- d21_2.py
+++ d21_2.py
@@ -1,4 +1,3 @@
-import itertools
 from dataclasses import dataclass
 
 dist_freqs = {
@@ -12,56 +11,40 @@ dist_freqs = {
 }
 
 
+# points_p1: int, points_p2: int, pos_p1: int, pos_p2: int
+State = tuple[int, int, int, int]
+
+
+def run(pos1: int, pos2: int, size: int = 10, limit: int = 21) -> int:
+    games_in_progress: dict[State, int] = {(0, 0, pos1, pos2): 1}
+    games_won = {1: 0, 2: 0}
+
+    while len(games_in_progress):
+        state, freq = games_in_progress.popitem()
+        for (p1, f1) in dist_freqs.items():
+            pos_p1 = ((state[2] - 1 + p1) % size) + 1
+            points_p1 = state[0] + pos_p1
+            if points_p1 >= limit:
+                games_won[1] += freq * f1
+                break
+
+            for (p2, f2) in dist_freqs.items():
+                pos_p2 = ((state[3] - 1 + p2) % size) + 1
+                points_p2 = state[1] + pos_p2
+                if points_p2 >= limit:
+                    games_won[2] += freq * f2
+                    break
+
+                new_state = (points_p1, points_p2, pos_p1, pos_p2)
+                if new_state not in games_in_progress:
+                    games_in_progress[new_state] = 1
+                games_in_progress[new_state] += f1 * f2
+    return max(games_won[1], games_won[2])
+
+
 def main() -> None:
     res = run(4, 6)
     print(res)
-
-
-@dataclass
-class State:
-    field: int
-    count: int
-    points: int
-    moves: int
-
-
-def get_all_results(pos: int, size: int, end_score: int) -> dict[int, int]:
-    in_progress: list[State] = [State(pos, 1, 0, 0)]
-    won: list[State] = []
-
-    while len(in_progress) > 0:
-        new_in_progress: list[State] = []
-        for ip in in_progress:
-            for dist_val, dist_freq in dist_freqs.items():
-                new_field = ((ip.field - 1 + dist_val) % size) + 1
-                new_points = ip.points + new_field
-                new_state = State(new_field, ip.count * dist_freq, new_points, ip.moves + 1)
-                if new_state.points >= end_score:
-                    won.append(new_state)
-                else:
-                    new_in_progress.append(new_state)
-        in_progress = new_in_progress
-    sorted(won, key=lambda w: w.moves)
-    kl: dict[int, list[State]] = {k: list(v) for k, v in itertools.groupby(won, lambda w: w.moves)}
-    res = {k: sum([s.count for s in v]) for k, v in kl.items()}
-    return res
-
-
-def run(p1_pos: int, p2_pos: int, size: int = 10, end_score: int = 21) -> int:
-    p1_res = get_all_results(p1_pos, size, end_score)
-    p2_res = get_all_results(p2_pos, size, end_score)
-
-    won_by_p1 = 0
-    for moves in p1_res:
-        p2_worse = sum([s for k, s in p2_res.items() if k >= moves])
-        won_by_p1 += p1_res[moves] * p2_worse
-
-    won_by_p2 = 0
-    for moves in p2_res:
-        p1_worse = sum([s for k, s in p1_res.items() if k > moves])
-        won_by_p2 += p2_res[moves] * p1_worse
-
-    return max([won_by_p1, won_by_p2])
 
 
 def test() -> None:
