db402f5ee806bc1deb1e13bca9d8eb3a06425492
Łukasz Piątkowski <piontec@gmail.com>
2022-12-04 10:45:17 +0100

    d21-2 again

--- d21_2.py
+++ d21_2.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from functools import cache
+
 
 dist_freqs = {
     3: 1,
@@ -10,9 +11,33 @@ dist_freqs = {
     9: 1,
 }
 
-
 # points_p1: int, points_p2: int, pos_p1: int, pos_p2: int
 State = tuple[int, int, int, int]
+
+
+@cache
+def get_wins(state: State, active: int) -> tuple[int, int]:
+    if state[active] >= 21:
+        return (1, 0) if active == 0 else (0, 1)
+    total_p1, total_p2 = 0, 0
+    for (p, f) in dist_freqs.items():
+        new_pos = ((state[2 + active] - 1 + p) % 10) + 1
+        new_points = state[0 + active] + new_pos
+
+        new_state = (new_points, state[1], new_pos, state[3]) if active == 0 else (state[0], new_points, state[2], new_pos)
+
+        p1_won, p2_won = get_wins(new_state, (active + 1) % 2)
+        total_p1 += f * p1_won
+        total_p2 += f * p2_won
+    return total_p1, total_p2
+
+
+def run2(pos1: int, pos2: int, size: int = 10, limit: int = 21) -> int:
+    state = (0, 0, pos1, pos2)
+
+    p1_won, p2_won = get_wins(state, active=0)
+    # FIXME
+    return max(p1_won, p2_won) // 27
 
 
 def run(pos1: int, pos2: int, size: int = 10, limit: int = 21) -> int:
@@ -24,31 +49,32 @@ def run(pos1: int, pos2: int, size: int = 10, limit: int = 21) -> int:
         for (p1, f1) in dist_freqs.items():
             pos_p1 = ((state[2] - 1 + p1) % size) + 1
             points_p1 = state[0] + pos_p1
-            if points_p1 >= limit:
-                games_won[1] += freq * f1
-                break
 
             for (p2, f2) in dist_freqs.items():
                 pos_p2 = ((state[3] - 1 + p2) % size) + 1
                 points_p2 = state[1] + pos_p2
-                if points_p2 >= limit:
-                    games_won[2] += freq * f2
-                    break
 
-                new_state = (points_p1, points_p2, pos_p1, pos_p2)
-                if new_state not in games_in_progress:
-                    games_in_progress[new_state] = 1
-                games_in_progress[new_state] += f1 * f2
+                new_freq = f1 * f2
+                if points_p1 >= limit:
+                    games_won[1] += freq * new_freq
+                elif points_p2 >= limit:
+                    games_won[2] += freq * new_freq
+                else:
+                    new_state = (points_p1, points_p2, pos_p1, pos_p2)
+                    if new_state not in games_in_progress:
+                        games_in_progress[new_state] = new_freq
+                    else:
+                        games_in_progress[new_state] *= new_freq
     return max(games_won[1], games_won[2])
 
 
 def main() -> None:
-    res = run(4, 6)
+    res = run2(4, 6)
     print(res)
 
 
 def test() -> None:
-    res = run(4, 8)
+    res = run2(4, 8)
     assert res == 444356092776315
 
 
